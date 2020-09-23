6a789363634af6078c019ca43e19b3e2b454b3cf
Brandon Stalnaker <bstalnaker@mparticle.com>
2020-09-23 13:35:59 -0400

    Add Additional StreamTypes

--- media/src/main/java/com/mparticle/media/events/StreamType.kt
+++ media/src/main/java/com/mparticle/media/events/StreamType.kt
@@ -3,4 +3,7 @@ package com.mparticle.media.events
 object StreamType {
     const val LIVE_STEAM = "LiveStream"
     const val ON_DEMAND = "OnDemand"
+    const val LINEAR = "Linear"
+    const val PODCAST = "Podcast"
+    const val AUDIOBOOK = "Audiobook"
 }
