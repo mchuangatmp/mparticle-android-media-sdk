908c6cf058e41e62a95082b6a383bf097e44d5a4
Will Passidomo <wpassidomo@mparticle.com>
2022-04-26 17:53:44 -0400

    chore: Upgrade AGP, Kotlin, Gradle, migrate to kts

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+include("media")
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+    plugins {
+        id("com.android.library") version "7.1.3"
+        id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+        kotlin("android") version "1.6.20"
+    }
+}
